d614be843f4c987636cfc5a877ddf2619aaae654
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-15 07:57:50 +0800

    mybatis注解开发

---
+++ mybatis_annotation/src/test/java/com/zyg/test/AnnotationCRUDTest.java
@@ -0,0 +1,45 @@
+package com.zyg.test;
+
+import com.zyg.dao.IUserDao;
+import com.zyg.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+
+public class AnnotationCRUDTest {
+    private InputStream in;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+    private IUserDao userDao;
+
+    @Before
+    public  void init()throws Exception{
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        factory = new SqlSessionFactoryBuilder().build(in);
+        session = factory.openSession();
+        userDao = session.getMapper(IUserDao.class);
+    }
+
+    @After
+    public  void destroy()throws  Exception{
+        session.commit();
+        session.close();
+        in.close();
+    }
+
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setUsername("mybatis annotation");
+        user.setAddress("CD");
+        userDao.saveUser(user);
+    }
+}
