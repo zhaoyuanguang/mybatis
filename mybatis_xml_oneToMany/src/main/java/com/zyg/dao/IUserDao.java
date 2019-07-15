feccc241aaacf25494e61c0379014e8956e779a4
zhaoyuanguang <zhaoyuanguang1573.@163.com>
2019-07-15 08:57:54 +0800

    mybatis_xml:一对多查询

---
+++ mybatis_xml_oneToMany/src/main/java/com/zyg/dao/IUserDao.java
@@ -0,0 +1,25 @@
+package com.zyg.dao;
+
+import com.zyg.domain.User;
+
+import java.util.List;
+
+
+public interface IUserDao {
+
+    /**
+     * 查询所有用户，同时获取到用户下所有账户的信息
+     * @return
+     */
+    List<User> findAll();
+
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+
+}
